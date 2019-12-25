7c8c732ddcb86c55e3335c50dd5ae9579e22f249
Yaroslav Dobzhanskij <merlyn.tgz@gmail.com>
2019-12-25 02:41:40 +0200

    lot of changes, mongoose options to plugion v1.0.0-beta.5

--- README.md
+++ README.md
@@ -13,9 +13,9 @@ Speed booster for multiple related models properties fetching & mixing to your m
  
 ## Workflow description 
 
-You form `subscription` around your **related model** with `makeSubscription(model, options)`
+You create `subscription` around your **related model** with `makeSubscription(model, options)`
 
-You can form any count of subscriptions, you need.
+You can create any count of subscriptions you need.
 
 Then you can to `subscription.add(target)` **target objects** you want to mix in 
 properties from **related model** data. 
@@ -26,13 +26,25 @@ After you've added all needed **targets** to all `subscriptions` you can anytime
 `fillSubscriptions()` assigns data as it goes via **stream** with auto **parallelization** 
 if multiple `subscriptions` created. One query per `subscription` is executed.
  
-It generates mongo condition to query your source, but this is just default 
-behavior
+It generates mongo condition. If you return from 
+`options.getCondition(target)` *scalar value* then is generated `$in` query. I   
+to query your source, 
+
+Mongo query generation is just default behavior, you can alter it as you want.
 
 ## Installation
 
 ```shell script
-npm i subscribe-for-data
+npm i subscribe-for-data subscribe-for-data-from-mongoose
+```
+
+## Import
+
+```javascript
+const mongoosePlugin = require('subscribe-for-data-from-mongoose');
+const { 
+  makeSubscription, fillSubscriptions 
+} = require('subscribe-for-data').use(mongoosePlugin);
 ```
 
 ## Example
@@ -41,7 +53,8 @@ By default it works with mongoose. This behavior can be easily overriden by
 setting custom `getStream` option callback.
 
 ```javascript
-const { makeSubscription, fillSubscriptions } = require('subscribe-for-data');
+const mongoosePlugin = require('subscribe-for-data-from-mongoose');
+const { makeSubscription, fillSubscriptions } = require('subscribe-for-data').use(mongoosePlugin);
 const RootModel = require('./MainModel');
 const RelatedModel = require('./RelatedModel');
 const AnotherRelatedModel = require('./AnotherRelatedModel');
@@ -82,3 +95,166 @@ Expected output:
   "otherTypes": [23, 42, 78]
 }
 ```
+
+# API Reference
+
+##
+
+* [SubscribeForData](#SubscribeForData) : <code>object</code>
+    * [.makeSubscription(source, options)](#SubscribeForData.makeSubscription) ⇒ <code>Object</code>
+    * [.fillSubscriptions()](#SubscribeForData.fillSubscriptions) ⇒ <code>Promise</code>
+    * [.assignDefaultOptions(mixin)](#SubscribeForData.assignDefaultOptions)
+
+##
+
+* [assignData](#assignData) : <code>function</code>
+* [getKey](#getKey) ⇒ <code>\*</code>
+* [extractKey](#extractKey) ⇒ <code>\*</code>
+* [getCondition](#getCondition) ⇒ <code>\*</code>
+* [getDataHandler](#getDataHandler) ⇒ <code>function</code>
+* [getAddingMethod](#getAddingMethod) ⇒ <code>function</code>
+* [getStream](#getStream) : <code>function</code>
+
+<a name="SubscribeForData"></a>
+
+## SubscribeForData : <code>object</code>
+subscribe-for-data
+
+
+* [SubscribeForData](#SubscribeForData) : <code>object</code>
+    * [.makeSubscription(source, options)](#SubscribeForData.makeSubscription) ⇒ <code>Object</code>
+    * [.fillSubscriptions()](#SubscribeForData.fillSubscriptions) ⇒ <code>Promise</code>
+    * [.assignDefaultOptions(mixin)](#SubscribeForData.assignDefaultOptions)
+
+<a name="SubscribeForData.makeSubscription"></a>
+
+### SubscribeForData.makeSubscription(source, options) ⇒ <code>Object</code>
+Creates subscription for related model data
+
+
+| Param | Type | Description |
+| --- | --- | --- |
+| source | <code>Object</code> | Source model |
+| options | <code>Object</code> | Options |
+| options.targetField | <code>String</code> | field data to be saved into (optional) |
+| options.baseCondition | <code>Object</code> | Base condition |
+| options.defaultValue | <code>\*</code> | Default value for field |
+| options.getKey | [<code>getKey</code>](#getKey) | Callback which returns unique key from target model (model.id by default) |
+| options.getCondition | [<code>getCondition</code>](#getCondition) | returns condition, using target model (model.id by default) |
+| options.extractKey | [<code>extractKey</code>](#extractKey) | returns unique key of target model from foreign model |
+| options.isMultiple | <code>Boolean</code> | if one to many relation |
+| options.useEachAsync | <code>Boolean</code> | only for mongoose cursor |
+| options.parallel | <code>Number</code> | parallel parameter for `eachAsync` if `useEachAsync` is `true` |
+| options.foreignField | <code>String</code> | If `getCondition` returns scalar values this field will be used for $in |
+| options.sourceField | <code>String</code> | field to use of foreign model |
+| options.assignData | [<code>assignData</code>](#assignData) | Do model filling by itself, otherwise use `targetField` |
+| options.getStream | [<code>getStream</code>](#getStream) | returns stream from source and condition (using mongoose model by default) |
+| options.getDataHandler | [<code>getDataHandler</code>](#getDataHandler) | Get data handler for processing related models |
+| options.getAddingMethod | [<code>getAddingMethod</code>](#getAddingMethod) | Get `add()` method of future `subscription` |
+
+<a name="SubscribeForData.fillSubscriptions"></a>
+
+### SubscribeForData.fillSubscriptions() ⇒ <code>Promise</code>
+Fill subscribed targets
+
+<a name="SubscribeForData.assignDefaultOptions"></a>
+
+### SubscribeForData.assignDefaultOptions(mixin)
+change default options
+
+
+| Param |
+| --- |
+| mixin |
+
+<a name="assignData"></a>
+
+## assignData : <code>function</code>
+Assigns data from foreign model to target
+
+
+| Param | Type | Description |
+| --- | --- | --- |
+| target | <code>Object</code> | your target model |
+| foreign | <code>Object</code> | foreign model |
+
+<a name="getKey"></a>
+
+## getKey ⇒ <code>\*</code>
+get unique identifier of target for internal indexing
+
+**Returns**: <code>\*</code> - target identifier
+
+| Param | Type | Description |
+| --- | --- | --- |
+| target | <code>Object</code> | your target model |
+
+<a name="extractKey"></a>
+
+## extractKey ⇒ <code>\*</code>
+get unique identifier of target from foreign model
+
+**Returns**: <code>\*</code> - target identifier
+
+| Param | Type | Description |
+| --- | --- | --- |
+| foreign | <code>Object</code> | Foreign model data |
+
+<a name="getCondition"></a>
+
+## getCondition ⇒ <code>\*</code>
+get condition
+
+**Returns**: <code>\*</code> - condition, can be scalar or object
+
+| Param | Type | Description |
+| --- | --- | --- |
+| target | <code>Object</code> | your target model |
+
+<a name="getDataHandler"></a>
+
+## getDataHandler ⇒ <code>function</code>
+get foreign data handler
+
+**Returns**: <code>function</code> - Callback handling data assignment
+
+| Param | Type | Description |
+| --- | --- | --- |
+| options | <code>Object</code> | Options |
+| options.targets | <code>Object</code> | targets index |
+| options.targetField | <code>String</code> | field data to be saved into |
+| options.extractKey | [<code>extractKey</code>](#extractKey) | returns unique key of target model from foreign model |
+| options.isMultiple | <code>Boolean</code> | if one to many relation |
+| options.sourceField | <code>String</code> | field to use of foreign model |
+| options.assignData | [<code>assignData</code>](#assignData) | Do model filling by itself, otherwise use `targetField` |
+
+<a name="getAddingMethod"></a>
+
+## getAddingMethod ⇒ <code>function</code>
+get future `subscription.add()` method
+
+**Returns**: <code>function</code> - Callback handling data assignment
+
+| Param | Type | Description |
+| --- | --- | --- |
+| options | <code>Object</code> | Options |
+| options.targets | <code>Object</code> | targets index |
+| options.getKey | [<code>getKey</code>](#getKey) | Callback which returns unique key from target model (model.id by default) |
+| options.getCondition | [<code>getCondition</code>](#getCondition) | returns condition, using target model (model.id by default) |
+| options.defaultValue | <code>\*</code> | Default value for field |
+| options.targetField | <code>String</code> | field data to be saved into |
+| options.condition | <code>object</code> | DB Query condition, being prepared |
+| options.extractKey | [<code>extractKey</code>](#extractKey) | returns unique key of target model from foreign model |
+| options.foreignField | <code>String</code> | If `getCondition` returns scalar values this field will be used for $in |
+| options.inner | <code>Array</code> | Internal array for condition storing |
+
+<a name="getStream"></a>
+
+## getStream : <code>function</code>
+get stream from model using condition
+
+
+| Param | Description |
+| --- | --- |
+| source | Source model |
+| condition | Query condition |
--- index.js
+++ index.js
@@ -1,19 +1,115 @@
+/**
+ * subscribe-for-data
+ * @module subscribe-for-data
+ * @namespace SubscribeForData
+ */
 const EventEmitter = require('events');
 
-const bus = new EventEmitter();
-let isFilling = false;
-const awaiting = [];
-const defaultOptions = {
-  getKey: item => item._id,
-  getCondition: item => item._id,
-  getStream: (source, condition) => source.find(condition).lean().cursor(),
-  getDataHandler: ({ targets, extractKey, isMultiple, targetField, sourceField }) => (foreign) => {
-    const target = targets[extractKey(foreign)];
+function use(defaultOptions) {
+  defaultOptions.parallel = defaultOptions.parallel || 1;
+  const bus = new EventEmitter();
+  let isFilling = false;
+  const awaiting = [];
 
-    if (assignData) return assignData(target, foreign);
-    if (!isMultiple) target[targetField] = foreign[sourceField];
-    else (target[targetField] = target[targetField] || []).push(foreign[sourceField]);
-  },
+  /**
+   * Creates subscription for related model data
+   *
+   * @param {Object} source Source model
+   * @param {Object} options Options
+   * @param {String} options.targetField field data to be saved into (optional)
+   * @param {Object} options.baseCondition Base condition
+   * @param {*} options.defaultValue Default value for field
+   * @param {getKey} options.getKey Callback which returns unique key from target model (model.id by default)
+   * @param {getCondition} options.getCondition returns condition, using target model (model.id by default)
+   * @param {extractKey} options.extractKey returns unique key of target model from foreign model
+   * @param {Boolean} options.isMultiple if one to many relation
+   * @param {Boolean} options.useEachAsync only for mongoose cursor
+   * @param {Number} options.parallel parallel parameter for `eachAsync` if `useEachAsync` is `true`
+   * @param {String} options.foreignField If `getCondition` returns scalar values this field will be used for $in
+   * @param {String} options.sourceField field to use of foreign model
+   * @param {assignData} options.assignData Do model filling by itself, otherwise use `targetField`
+   * @param {getStream} options.getStream returns stream from source and condition (using mongoose model by default)
+   * @param {getDataHandler} options.getDataHandler Get data handler for processing related models
+   * @param {getAddingMethod} options.getAddingMethod Get `add()` method of future `subscription`
+   * @memberOf SubscribeForData
+   * @returns {Object}
+   */
+  function makeSubscription(source, options) {
+    const {
+      getKey, getCondition, foreignField, baseCondition, defaultValue, targetField, getAddingMethod
+    } = Object.assign({}, options, defaultOptions);
+    options.extractKey = options.extractKey || (foreign => foreign[foreignField]);
+    const targets = {};
+    const condition = baseCondition || {};
+    const inner = [];
+    let isConditionSetup = false;
+    awaiting.push({ source, targetField, options, targets, condition });
+
+    return {
+      add: getAddingMethod({
+        targets, getKey, getCondition, defaultValue, targetField, condition, foreignField, inner, isConditionSetup,
+      }),
+    };
+  }
+
+  /**
+   * Fill subscribed targets
+   *
+   * @memberOf SubscribeForData
+   * @returns {Promise}
+   */
+  async function fillSubscriptions() {
+    if (isFilling) await new Promise(resolve => bus.once('released', resolve));
+
+    isFilling = true;
+    const promises = awaiting
+      .map(({ source, targetField, options: {
+          extractKey, assignData, sourceField, getStream, isMultiple, useEachAsync, getDataHandler, parallel,
+        }, targets, condition }) => useEachAsync ?
+        getStream(source, condition).eachAsync(getDataHandler({ targets, extractKey, isMultiple, targetField, sourceField }),
+          { parallel }) :
+        new Promise((resolve, reject) => getStream(source, condition)
+          .on('data', getDataHandler({ targets, extractKey, isMultiple, targetField, sourceField }))
+          .on('error', reject)
+          .on('end', resolve)));
+    awaiting.splice(0, awaiting.length);
+    isFilling = false;
+    bus.emit('released');
+
+    return Promise.all(promises);
+  }
+
+  /**
+   * change default options
+   *
+   * @memberOf SubscribeForData
+   * @param mixin
+   */
+  function assignDefaultOptions(mixin) {
+    Object.assign(defaultOptions, mixin);
+  }
+
+  return {
+    fillSubscriptions,
+    makeSubscription,
+    assignDefaultOptions,
+  };
+
+}
+
+module.exports = {
+  /**
+   * Creates module instance
+   *
+   * @param {Object} options Options
+   * @param {getKey} options.getKey Callback which returns unique key from target model (model.id by default)
+   * @param {getStream} options.getStream returns stream from source and condition (using mongoose model by default)
+   * @param {getCondition} options.getCondition returns condition, using target model (model.id by default)
+   * @param {getDataHandler} options.getDataHandler Get data handler for processing related models
+   * @param {getAddingMethod} options.getAddingMethod Get `add()` method of future `subscription`
+   * @returns {SubscribeForData}
+   */
+  use,
 };
 
 /**
@@ -21,87 +117,62 @@ const defaultOptions = {
  *
  * @callback assignData
  * @param {Object} target your target model
- * @param {Object} foreign foreign data
+ * @param {Object} foreign foreign model
  */
-
 /**
+ * get unique identifier of target for internal indexing
  *
- * @param {Function} source Function to run on filling needed, it must return mongoose `Query`
- * @param {String} options.targetField field data to be saved into (optional)
- * @param {Object} options Options
- * @param {Object} options.baseCondition Base condition
- * @param {mixed} options.defaultValue Default value for field
- * @param {Function} options.getKey Callback which returns unique key from target model (model.id by default)
- * @param {Function} options.getStream returns stream from source and condition (using mongoose model by default)
- * @param {Function} options.getCondition returns condition, using target model (model.id by default)
- * @param {Function} options.extractKey returns unique key of target model from foreign model
- * @param {Boolean} options.isMultiple if one to many relation
- * @param {Boolean} options.useEachAsync only for mongoose cursor
- * @param {String} options.foreignField If `getCondition` returns scalar values this field will be used for $in
- * @param {String} options.sourceField field to use of foreign model
- * @param {assignData} options.assignData (optional) Do model filling by itself, otherwise use `targetField`
- * @returns {Object}
+ * @callback getKey
+ * @param {Object} target your target model
+ * @return {*} target identifier
  */
-function makeSubscription(source, options) {
-  const {
-    getKey, getCondition, foreignField, baseCondition, defaultValue, targetField,
-  } = Object.assign({}, options, defaultOptions);
-  options.extractKey = options.extractKey || (foreign => foreign[foreignField]);
-  const targets = {};
-  const condition = baseCondition || {};
-  const inner = [];
-  let isConditionSetup = false;
-  awaiting.push({ source, targetField, options, targets, condition });
-
-  return {
-    add(target) {
-      Object.assign(targets, { [getKey(target)]: target });
-      const itemCondition = getCondition(target);
-
-      if (defaultValue) if (typeof defaultValue !== 'object') target[targetField] = defaultValue;
-      else Object.assign(target, defaultValue);
-      if (!isConditionSetup) {
-        if (typeof itemCondition === 'object') condition.$or = condition.$or || inner;
-        else condition[foreignField] = condition[foreignField] || { $in: inner };
-
-        isConditionSetup = true;
-      }
-
-      inner.push(itemCondition);
-    },
-  };
-}
-
 /**
- * Fill subscribed targets
- * @returns {Promise}
+ * get unique identifier of target from foreign model
+ *
+ * @callback extractKey
+ * @param {Object} foreign Foreign model data
+ * @return {*} target identifier
  */
-async function fillSubscriptions() {
-  if (isFilling) await new Promise(resolve => bus.once('released', resolve));
-
-  isFilling = true;
-  const promises = awaiting
-    .map(({
-      source, targetField, options: {
-        extractKey, assignData, sourceField, getStream, isMultiple, useEachAsync, getDataHandler,
-      }, targets, condition,
-    }) => useEachAsync ?
-      getStream(source, condition).eachAsync(getDataHandler({ targets, extractKey, isMultiple, targetField, sourceField })) :
-      new Promise((resolve, reject) => getStream(source, condition)
-        .on('data', getDataHandler({ targets, extractKey, isMultiple, targetField, sourceField }))
-        .on('error', reject)
-        .on('end', resolve)));
-  awaiting.splice(0, awaiting.length);
-  isFilling = false;
-  bus.emit('released');
-
-  return Promise.all(promises);
-}
-
-module.exports = {
-  fillSubscriptions,
-  makeSubscription,
-  assignDefaultOptions(mixin) {
-    Object.assign(defaultOptions, mixin);
-  },
-};
+/**
+ * get condition
+ *
+ * @callback getCondition
+ * @param {Object} target your target model
+ * @return {*} condition, can be scalar or object
+ */
+/**
+ * get foreign data handler
+ *
+ * @callback getDataHandler
+ * @param {Object} options Options
+ * @param {Object} options.targets targets index
+ * @param {String} options.targetField field data to be saved into
+ * @param {extractKey} options.extractKey returns unique key of target model from foreign model
+ * @param {Boolean} options.isMultiple if one to many relation
+ * @param {String} options.sourceField field to use of foreign model
+ * @param {assignData} options.assignData Do model filling by itself, otherwise use `targetField`
+ * @return {Function} Callback handling data assignment
+ */
+/**
+ * get future `subscription.add()` method
+ *
+ * @callback getAddingMethod
+ * @param {Object} options Options
+ * @param {Object} options.targets targets index
+ * @param {getKey} options.getKey Callback which returns unique key from target model (model.id by default)
+ * @param {getCondition} options.getCondition returns condition, using target model (model.id by default)
+ * @param {*} options.defaultValue Default value for field
+ * @param {String} options.targetField field data to be saved into
+ * @param {object} options.condition DB Query condition, being prepared
+ * @param {extractKey} options.extractKey returns unique key of target model from foreign model
+ * @param {String} options.foreignField If `getCondition` returns scalar values this field will be used for $in
+ * @param {Array} options.inner Internal array for condition storing
+ * @return {Function} Callback handling data assignment
+ */
+/**
+ * get stream from model using condition
+ *
+ * @callback getStream
+ * @param source Source model
+ * @param condition Query condition
+ */
